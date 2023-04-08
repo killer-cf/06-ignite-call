4aaddb4e1454bedf60c4cdd8906d6492d2cc4396
killer-cf <costa.kilder@live.com>
2023-04-08 20:53:09 -0300

    style: component claimUsernameForm

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Container, Hero, Preview } from './styles'
 
 import previewImage from '../../assets/app-preview.png'
+import { ClaimUserNameForm } from './components/ClaimUserNameForm'
 
 export default function Home() {
   return (
@@ -13,6 +14,8 @@ export default function Home() {
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </Text>
+
+        <ClaimUserNameForm />
       </Hero>
 
       <Preview>
