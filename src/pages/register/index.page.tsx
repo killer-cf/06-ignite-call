ff97046351ba100559674c84087415430048009c
killer-cf <costa.kilder@live.com>
2023-04-10 16:18:37 -0300

    feat: create new user on db

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -7,6 +7,7 @@ import { z } from 'zod'
 import { Container, Form, FormError, Header } from './styles'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '@/lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -41,7 +42,16 @@ export default function Register() {
     }
   }, [router.query?.username, setValue])
 
-  async function handleRegister(data: RegisterFormData) {}
+  async function handleRegister(data: RegisterFormData) {
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <Container>
